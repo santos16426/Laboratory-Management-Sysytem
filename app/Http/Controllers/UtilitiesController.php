d330caeb220bb9cb7e69e50ab9944473bf585e35
Billy <santos16426@gmail.com>
2017-10-05 02:36:11 +0800

    Utilities

--- app/Http/Controllers/UtilitiesController.php
+++ app/Http/Controllers/UtilitiesController.php
@@ -9,7 +9,21 @@ class UtilitiesController extends Controller
 {
     function reactivation()
     {
-    	return view('Utilities.Reactivation');
+    	$lab = DB::table('laboratory_tbl')->where('LabStatus',0)->get();
+    	$emptype = DB::table('employee_role_tbl')
+    		->leftjoin('laboratory_tbl','laboratory_tbl.lab_id','=','employee_role_tbl.lab_id')
+    		->where('RoleStatus',0)
+    		->get();
+    	$emp = DB::table('employee_tbl')->where('EmpStatus',0)->get();
+    	$servgroup = DB::table('service_group_tbl')->where('ServGroupStatus',0)->get();
+    	$servtype = DB::table('service_type_tbl')->where('ServTypeStatus',0)->get();
+    	$serv = DB::table('service_tbl')->where('ServiceStatus',0)->get();
+    	$pack = DB::table('package_tbl')->where('PackStatus',0)->get();
+    	$corp = DB::table('corporate_accounts_tbl')->where('CorpStatus',0)->get();
+    	$corppack = DB::table('corp_package_tbl')
+    		->leftjoin('corporate_accounts_tbl','corporate_accounts_tbl.corp_id','=','corp_package_tbl.corp_id')
+    		->where('CorpPackStatus',0)->get();
+    	return view('Utilities.Reactivation',['lab'=>$lab,'emptype'=>$emptype,'emp'=>$emp,'servgroup'=>$servgroup,'servtype'=>$servtype,'serv'=>$serv,'pack'=>$pack,'corp'=>$corp,'corppack'=>$corppack]);
     }
     function companydetails()
     {
--- resources/views/Utilities/Reactivation.blade.php
+++ resources/views/Utilities/Reactivation.blade.php
@@ -12,149 +12,367 @@
 @section('reactivation','active')
 @section('utilitiesactive','active')
 @section('content')
-<div class="row">
-	<div class="col-lg-6">
-		<section class="panel">
-			<header class="panel-heading">
-				Laboratory
-				<span class="tools pull-right">
-		          <a class="fa fa-chevron-down" href="javascript:;"></a>
-		      	</span>
-			</header>
-			<div class="panel-body">
-				<div class="clearfix">
-					
-				</div>
-			</div>
-		</section>
-	</div>
 
-	<div class="col-lg-6">
-		<section class="panel">
-			<header class="panel-heading">
-				Employee Type
-				<span class="tools pull-right">
-		          <a class="fa fa-chevron-down" href="javascript:;"></a>
-		      	</span>
-			</header>
-			<div class="panel-body">
-				<div class="clearfix">
-					
-				</div>
-			</div>
-		</section>
-	</div>
+<div class="col-md-12">
+	<section class="panel">
+		<header class="panel-heading btn-info ">
+      <ul class="nav nav-tabs">
+          <li class="active">
+              <a data-toggle="tab" href="#labs">Laboratory</a>
+          </li>
+          <li class="">
+              <a data-toggle="tab" href="#emptype">Employee Type</a>
+          </li>
+          <li class="">
+              <a data-toggle="tab" href="#emp">Employee</a>
+          </li>
+          <li class="">
+              <a data-toggle="tab" href="#servgroup">Service Group</a>
+          </li>
+          <li class="">
+              <a data-toggle="tab" href="#servtype">Service Type</a>
+          </li>
+          <li class="">
+              <a data-toggle="tab" href="#serv">Service</a>
+          </li>
+          <li class="">
+              <a data-toggle="tab" href="#pack">Package</a>
+          </li>
+          <li class="">
+              <a data-toggle="tab" href="#corp">Corporate Account</a>
+          </li>
+          <li class="">
+              <a data-toggle="tab" href="#corppack">Corporate Account Package</a>
+          </li>
 
-	<div class="col-lg-6">
-		<section class="panel">
-			<header class="panel-heading">
-				Employee
-				<span class="tools pull-right">
-		          <a class="fa fa-chevron-down" href="javascript:;"></a>
-		      	</span>
-			</header>
-			<div class="panel-body">
-				<div class="clearfix">
-					
-				</div>
-			</div>
-		</section>
-	</div>
+      </ul>
+  	</header>
+  	<div class="panel-body">
+  		<div class="tab-content">
+        <div id="labs" class="tab-pane active">
+            <table class="table table-bordered table-hover dataTable" id="labTbl">
+				      <thead>
+				        <tr>
+				          <th>Laboratory Name</th>
+				          <th>Action</th>
+				        </tr>
+				      </thead>
+				      <tbody>
+				        @foreach($lab as $labs)
+				        <tr>
+				        	<td>{{ $labs->lab_name }}</td>
+				        	<td>
+				        		<button type="button" class="btn btn-success btn-xs labbtn" data-id="{{ $labs->lab_id }}"><i class="fa fa-recycle" aria-hidden="true"></i>&nbsp; Reactivate</button>
+				        	</td>
+				        </tr>
+				        @endforeach
+				      </tbody>
+				    </table>
+        </div>
 
-	<div class="col-lg-6">
-		<section class="panel">
-			<header class="panel-heading">
-				Service Group
-				<span class="tools pull-right">
-		          <a class="fa fa-chevron-down" href="javascript:;"></a>
-		      	</span>
-			</header>
-			<div class="panel-body">
-				<div class="clearfix">
-					
-				</div>
-			</div>
-		</section>
-	</div>
+        <div class="tab-pane" id="emptype">
+        	<table class="table table-bordered table-hover dataTable" id="emptypetbl">
+			      <thead>
+			        <tr>
+			          <th>Employee Type Name</th>
+			          <th>Action</th>
+			        </tr>
+			      </thead>
+			      <tbody>
+			        @foreach($emptype as $emptypes)
+			        <tr>
+			        	<td>{{ $emptypes->role_name }}</td>
+			        	<td>
+			        		<button type="button" class="btn btn-success btn-xs emptypebtn" data-id="{{ $emptypes->role_id }}"><i class="fa fa-recycle" aria-hidden="true"></i>&nbsp; Reactivate</button>
+			        	</td>
+			        </tr>
+			        @endforeach
+			      </tbody>
+			    </table>
+        </div>
 
-	<div class="col-lg-6">
-		<section class="panel">
-			<header class="panel-heading">
-				Service Type
-				<span class="tools pull-right">
-		          <a class="fa fa-chevron-down" href="javascript:;"></a>
-		      	</span>
-			</header>
-			<div class="panel-body">
-				<div class="clearfix">
-					
-				</div>
-			</div>
-		</section>
-	</div>
+        <div class="tab-pane" id="emp">
+        	<table class="table table-bordered table-hover dataTable" id="emptbl">
+			      <thead>
+			        <tr>
+			          <th>Employee Name</th>
+			          <th>Action</th>
+			        </tr>
+			      </thead>
+			      <tbody>
+			        @foreach($emp as $emps)
+			        <tr>
+			        	<td>{{ $emps->emp_fname }} {{ $emps->emp_mname }} {{ $emps->emp_lname }}</td>
+			        	<td>
+			        		<button type="button" class="btn btn-success btn-xs empbtn" data-id="{{ $emps->emp_id }}"><i class="fa fa-recycle" aria-hidden="true"></i>&nbsp; Reactivate</button>
+			        	</td>
+			        </tr>
+			        @endforeach
+			      </tbody>
+			    </table>
+        </div>
 
-	<div class="col-lg-6">
-		<section class="panel">
-			<header class="panel-heading">
-				Service
-				<span class="tools pull-right">
-		          <a class="fa fa-chevron-down" href="javascript:;"></a>
-		      	</span>
-			</header>
-			<div class="panel-body">
-				<div class="clearfix">
-					
-				</div>
-			</div>
-		</section>
-	</div>
+        <div class="tab-pane" id="servgroup">
+        	<table class="table table-bordered table-hover dataTable" id="servgrouptbl">
+			      <thead>
+			        <tr>
+			          <th>Service Group Name</th>
+			          <th>Action</th>
+			        </tr>
+			      </thead>
+			      <tbody>
+			        @foreach($servgroup as $servgroup)
+			        <tr>
+			        	<td>{{ $servgroup->servgroup_name }}</td>
+			        	<td>
+			        		<button type="button" class="btn btn-success btn-xs sergroupbtn" data-id="{{ $servgroup->servgroup_id }}"><i class="fa fa-recycle" aria-hidden="true"></i>&nbsp; Reactivate</button>
+			        	</td>
+			        </tr>
+			        @endforeach
+			      </tbody>
+			    </table>
+        </div>
 
-	<div class="col-lg-6">
-		<section class="panel">
-			<header class="panel-heading">
-				Package
-				<span class="tools pull-right">
-		          <a class="fa fa-chevron-down" href="javascript:;"></a>
-		      	</span>
-			</header>
-			<div class="panel-body">
-				<div class="clearfix">
-					
-				</div>
-			</div>
-		</section>
-	</div>
+        <div class="tab-pane" id="servtype">
+        	<table class="table table-bordered table-hover dataTable" id="servtypetbl">
+			      <thead>
+			        <tr>
+			          <th>Service Type Name</th>
+			          <th>Action</th>
+			        </tr>
+			      </thead>
+			      <tbody>
+			        @foreach($servtype as $servtype)
+			        <tr>
+			        	<td>{{ $servtype->service_type_name }}</td>
+			        	<td>
+			        		<button type="button" class="btn btn-success btn-xs sertypebtn" data-id="{{ $servtype->service_type_id }}"><i class="fa fa-recycle" aria-hidden="true"></i>&nbsp; Reactivate</button>
+			        	</td>
+			        </tr>
+			        @endforeach
+			      </tbody>
+			    </table>
+        </div>
 
-	<div class="col-lg-6">
-		<section class="panel">
-			<header class="panel-heading">
-				Corporate Accounts
-				<span class="tools pull-right">
-		          <a class="fa fa-chevron-down" href="javascript:;"></a>
-		      	</span>
-			</header>
-			<div class="panel-body">
-				<div class="clearfix">
-					
-				</div>
-			</div>
-		</section>
-	</div>
+        <div class="tab-pane" id="serv">
+        	<table class="table table-bordered table-hover dataTable" id="servtbl">
+			      <thead>
+			        <tr>
+			          <th>Service Name</th>
+			          <th>Action</th>
+			        </tr>
+			      </thead>
+			      <tbody>
+			        @foreach($serv as $serv)
+			        <tr>
+			        	<td>{{ $serv->service_name }}</td>
+			        	<td>
+			        		<button type="button" class="btn btn-success btn-xs serbtn" data-id="{{ $serv->service_id }}"><i class="fa fa-recycle" aria-hidden="true"></i>&nbsp; Reactivate</button>
+			        	</td>
+			        </tr>
+			        @endforeach
+			      </tbody>
+			    </table>
+        </div>
 
-	<div class="col-lg-6">
-		<section class="panel">
-			<header class="panel-heading">
-				Corporate Account Package
-				<span class="tools pull-right">
-		          <a class="fa fa-chevron-down" href="javascript:;"></a>
-		      	</span>
-			</header>
-			<div class="panel-body">
-				<div class="clearfix">
-					
-				</div>
-			</div>
-		</section>
-	</div>
+        <div class="tab-pane" id="pack">
+        	<table class="table table-bordered table-hover dataTable" id="packtbl">
+			      <thead>
+			        <tr>
+			          <th>Package Name</th>
+			          <th>Action</th>
+			        </tr>
+			      </thead>
+			      <tbody>
+			        @foreach($pack as $pack)
+			        <tr>
+			        	<td>{{ $pack->pack_name }}</td>
+			        	<td>
+			        		<button type="button" class="btn btn-success btn-xs packbtn" data-id="{{ $pack->pack_id }}"><i class="fa fa-recycle" aria-hidden="true"></i>&nbsp; Reactivate</button>
+			        	</td>
+			        </tr>
+			        @endforeach
+			      </tbody>
+			    </table>
+        </div>
+
+        <div class="tab-pane" id="corp">
+        	<table class="table table-bordered table-hover dataTable" id="corptbl">
+			      <thead>
+			        <tr>
+			          <th>Corporate Account</th>
+			          <th>Action</th>
+			        </tr>
+			      </thead>
+			      <tbody>
+			        @foreach($corp as $corp)
+			        <tr>
+			        	<td>{{ $corp->corp_name }}</td>
+			        	<td>
+			        		<button type="button" class="btn btn-success btn-xs corpbtn" data-id="{{ $corp->corp_id }}"><i class="fa fa-recycle" aria-hidden="true"></i>&nbsp; Reactivate</button>
+			        	</td>
+			        </tr>
+			        @endforeach
+			      </tbody>
+			    </table>
+        </div>
+
+        <div class="tab-pane" id="corppack">
+        	<table class="table table-bordered table-hover dataTable" id="corppacktbl">
+			      <thead>
+			        <tr>
+			          <th>Corporate Package Name</th>
+			          <th>Corporate Account Name</th>
+			          <th>Action</th>
+			        </tr>
+			      </thead>
+			      <tbody>
+			        @foreach($corppack as $corppack)
+			        <tr>
+			        	<td>{{ $corppack->corpPack_name }}</td>
+			        	<td>{{ $corppack->corp_name }}</td>
+			        	<td>
+			        		<button type="button" class="btn btn-success btn-xs corppackbtn" data-id="{{ $corppack->corp_id }}"><i class="fa fa-recycle" aria-hidden="true"></i>&nbsp; Reactivate</button>
+			        	</td>
+			        </tr>
+			        @endforeach
+			      </tbody>
+			    </table>
+        </div>
+    	</div>
+  	</div>
+	</section>
 </div>
+
+<div class="modal fade" id = "labmodal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header btn-success">
+        <h4 class="modal-title"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i> Reactivate Record</h4>
+      </div>
+      <form method="post" action="/activateLab" id="activateLab">
+      {{ csrf_field()  }}
+        <div class="modal-body">
+          <h4></h4>
+          <input type="text" class="hidden" name="id" id="labid" value="">
+          Are you sure you want to restore this record?
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-xs pull-left" data-dismiss="modal">Close</button>
+          <button  class="btn btn-xs btn-success" type="submit"><i class="fa fa-recycle" aria-hidden="true"></i>&nbsp;Activate</button>
+        </div>
+      </form>
+    </div>  
+  </div>
+</div>
+@endsection
+@section('additional')
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('.labbtn').click(function(){
+			var id = $(this).data('id');
+			$('#labid').val(id);
+			$('#labmodal').modal('show');
+		});
+		$('.emptypebtn').click(function(){
+			var id = $(this).data('id');
+		});
+		$('.empbtn').click(function(){
+			var id = $(this).data('id');
+		});
+		$('.sergroupbtn').click(function(){
+			var id = $(this).data('id');
+		});
+		$('.sertypebtn').click(function(){
+			var id = $(this).data('id');
+		});
+		$('.serbtn').click(function(){
+			var id = $(this).data('id');
+		});
+		$('.packbtn').click(function(){
+			var id = $(this).data('id');
+		});
+		$('.corpbtn').click(function(){
+			var id = $(this).data('id');
+		});
+		$('.corppackbtn').click(function(){
+			var id = $(this).data('id');
+		});
+  	$('#labTbl').dataTable({
+	    'paging'      : true,
+	    'lengthChange': true,
+	    'searching'   : true,
+	    'ordering'    : true,
+	    'info'        : true,
+	    'autoWidth'   : true,
+  	});
+  	$('#emptypetbl').dataTable({
+	    'paging'      : true,
+	    'lengthChange': true,
+	    'searching'   : true,
+	    'ordering'    : true,
+	    'info'        : true,
+	    'autoWidth'   : true,
+	  });
+	  $('#emptbl').dataTable({
+	    'paging'      : true,
+	    'lengthChange': true,
+	    'searching'   : true,
+	    'ordering'    : true,
+	    'info'        : true,
+	    'autoWidth'   : true,
+	  });
+	  $('#servgrouptbl').dataTable({
+	    'paging'      : true,
+	    'lengthChange': true,
+	    'searching'   : true,
+	    'ordering'    : true,
+	    'info'        : true,
+	    'autoWidth'   : true,
+	  });
+	  $('#servtypetbl').dataTable({
+	    'paging'      : true,
+	    'lengthChange': true,
+	    'searching'   : true,
+	    'ordering'    : true,
+	    'info'        : true,
+	    'autoWidth'   : true,
+	  });
+	  $('#servtbl').dataTable({
+	    'paging'      : true,
+	    'lengthChange': true,
+	    'searching'   : true,
+	    'ordering'    : true,
+	    'info'        : true,
+	    'autoWidth'   : true,
+	  });
+	  $('#packtbl').dataTable({
+	    'paging'      : true,
+	    'lengthChange': true,
+	    'searching'   : true,
+	    'ordering'    : true,
+	    'info'        : true,
+	    'autoWidth'   : true,
+	  });
+	  $('#corptbl').dataTable({
+	    'paging'      : true,
+	    'lengthChange': true,
+	    'searching'   : true,
+	    'ordering'    : true,
+	    'info'        : true,
+	    'autoWidth'   : true,
+	  });
+	  $('#corppacktbl').dataTable({
+	    'paging'      : true,
+	    'lengthChange': true,
+	    'searching'   : true,
+	    'ordering'    : true,
+	    'info'        : true,
+	    'autoWidth'   : true,
+	  });
+  });
+</script>
+
+
+
 @endsection
