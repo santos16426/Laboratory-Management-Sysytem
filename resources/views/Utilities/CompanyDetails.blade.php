9759e939ee53cecf0410310d4548d5bf7caffa07
mark <shirogawa23@gmail.com>
2017-10-09 04:01:12 +0800

    utiltil

--- resources/views/Utilities/CompanyDetails.blade.php
+++ resources/views/Utilities/CompanyDetails.blade.php
@@ -17,4 +17,110 @@
 @section('companydetails','active')
 @section('utilitiesactive','active')
 @section('content')
+
+<style type="text/css">
+  h2 {
+    font-family: Verdana 
+  }
+</style>
+
+<section class="panel">
+<div class="bio-graph-heading">
+    <center><strong><h2>Company Details</h2></strong></center>
+</div><br><br>
+<div class="panel-body bio-graph-info">
+    <div class="row">
+       <div class="col-md-12">
+        <div class="col-md-6">
+              <div class="form-group">
+                    <div class="col-md-12">
+                       <div class="input-group">
+                        <div class="input-group-addon">
+                         Company Name 
+                       </div>
+                      <input name="name" id="name" type="text" placeholder="Name" class="form-control input-md" required>
+                   </div>
+                </div>  
+             </div><br><br>
+         </div>
+         <div class="col-md-6">
+              <div class="form-group">
+                    <div class="col-md-12">
+                       <div class="input-group">
+                        <div class="input-group-addon">
+                        Tel No.
+                       </div>
+                      <input  name="dateofexam" id="dateofexam" placeholder="Date of Examination" class="form-control input-md" required>
+                   </div>
+                </div>  
+             </div><br><br>
+            </div> 
+        </div> 
+
+        <div class="col-md-12">
+        <div class="col-md-6">
+              <div class="form-group">
+                    <div class="col-md-12">
+                       <div class="input-group">
+                        <div class="input-group-addon">
+                         Residential Address 
+                       </div>
+                      <input name="name" id="name" type="text" placeholder="Name" class="form-control input-md" required>
+                   </div>
+                </div>  
+             </div><br><br>
+         </div>
+         <div class="col-md-6">
+              <div class="form-group">
+                    <div class="col-md-12">
+                       <div class="input-group">
+                        <div class="input-group-addon">
+                        Billing Address
+                       </div>
+                      <input  name="dateofexam" id="dateofexam" placeholder="Date of Examination" class="form-control input-md" required>
+                   </div>
+                </div>  
+             </div><br><br>
+            </div> 
+        </div> 
+
+        <div class="col-md-12">
+        <div class="col-md-6">
+              <div class="form-group">
+                    <div class="col-md-12">
+                       <div class="input-group">
+                        <div class="input-group-addon">
+                         Branch 
+                       </div>
+                      <input name="name" id="name" type="text" placeholder="Name" class="form-control input-md" required>
+                   </div>
+                </div>  
+             </div><br><br>
+         </div>
+         <div class="col-md-6">
+              <div class="form-group">
+                    <div class="col-md-12">
+                       <div class="input-group">
+                        <div class="input-group-addon">
+                        Email Address
+                       </div>
+                      <input  name="dateofexam" id="dateofexam" placeholder="Date of Examination" class="form-control input-md" required>
+                   </div>
+                </div>  
+             </div><br><br>
+            </div> 
+        </div>  
+
+        <div class="col-md-12">
+              <div class="form-group">
+                    <div class="col-md-12">
+                      <label>Description</label>
+                      <textarea  name="history" id="history" type="text" placeholder="" class="form-control input-md" required>
+                      </textarea>
+                </div>  
+             </div><br><br><br>
+        </div>  
+        </div>
+    </div>
+</section>
 @endsection
