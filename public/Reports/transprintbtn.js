22debca34a56b75921b4269360948cd820a74f92
Billy <santos16426@gmail.com>
2017-10-15 03:45:39 +0800

    dsas

--- public/Reports/transprintbtn.js
+++ public/Reports/transprintbtn.js
@@ -1,5 +1,8 @@
 $('#printbtn').click(function(){
 	var report = $('#selectrange').val();
+	var start_date = $('#start_date_date').val();
+	var monthly = $('#monthly_date').val();
+	var yearly = $('#yearly_date').val();
 	if(report == 'daily')
 		{
 			if(start_date != '')
