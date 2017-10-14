654803ce9e0fd9754aa84567aef4b9d48dae4b81
Billy <santos16426@gmail.com>
2017-10-14 23:23:33 +0800

    census report print

--- public/Reports/censusprintbtn.js
+++ public/Reports/censusprintbtn.js
@@ -1,8 +1,305 @@
 $('#printbtn').click(function(){
-	
 	var report = $('#selectrange').val();
-    var start_date = $('#start_date_date').val();
-    var monthly = $('#monthly_date').val();
-    var yearly = $('#yearly_date').val();
-        
+	if(report == 'daily')
+		{
+			if(start_date != '')
+			{		
+				$.ajax
+				({
+					url: '/dailyCensusReport',
+					type: 'get',
+					data:  { start_date:start_date},
+					dataType : 'json',
+					success:function(response){
+						toastr.options = {
+					      "closeButton": true,
+					      "debug": false,
+					      "positionClass": "toast-top-right",
+					      "onclick": null,
+					      "showDuration": "3000",
+					      "hideDuration": "100",
+					      "timeOut": "3000",
+					      "extendedTimeOut": "0",
+					      "showEasing": "swing",
+					      "hideEasing": "swing",
+					      "showMethod": "show",
+					      "hideMethod": "hide"
+					    }
+					    toastr.success("Done!");
+					    
+					},
+					error:function()
+					{
+						toastr.options = {
+					      "closeButton": true,
+					      "debug": false,
+					      "positionClass": "toast-top-right",
+					      "onclick": null,
+					      "showDuration": "3000",
+					      "hideDuration": "100",
+					      "timeOut": "3000",
+					      "extendedTimeOut": "0",
+					      "showEasing": "swing",
+					      "hideEasing": "swing",
+					      "showMethod": "show",
+					      "hideMethod": "hide"
+					    }
+					    toastr.error("Error! Please try again later");
+					}
+				});
+				
+			}
+			else
+			{
+				toastr.options = {
+			      "closeButton": true,
+			      "debug": false,
+			      "positionClass": "toast-top-right",
+			      "onclick": null,
+			      "showDuration": "3000",
+			      "hideDuration": "100",
+			      "timeOut": "3000",
+			      "extendedTimeOut": "0",
+			      "showEasing": "swing",
+			      "hideEasing": "swing",
+			      "showMethod": "show",
+			      "hideMethod": "hide"
+			    }
+			    toastr.warning("Please select a starting date");
+			}
+		}
+		else if(report == 'weekly')
+		{
+			if(start_date != '')
+			{
+			    var date = new Date(start_date);
+			    var newdate = new Date(date);
+			    var sdd = date.getDate();
+			    var smm = date.getMonth() + 1;
+			    var sy = date.getFullYear();
+			    var startdate = sy + '-' + smm + '-' + sdd;
+			    newdate.setDate(newdate.getDate() + 6);			    
+			    var dd = newdate.getDate();
+			    var mm = newdate.getMonth() + 1;
+			    var y = newdate.getFullYear();
+			    var enddate =  y + '-' + mm + '-' + dd ;
+				$.ajax
+				({
+					url: '/weeklyCensusReports',
+					type: 'get',
+					data:  { 
+						startdate:startdate,
+						enddate:enddate
+					},
+					dataType : 'json',
+
+					success:function(response){
+						
+						toastr.options = {
+					      "closeButton": true,
+					      "debug": false,
+					      "positionClass": "toast-top-right",
+					      "onclick": null,
+					      "showDuration": "3000",
+					      "hideDuration": "100",
+					      "timeOut": "3000",
+					      "extendedTimeOut": "0",
+					      "showEasing": "swing",
+					      "hideEasing": "swing",
+					      "showMethod": "show",
+					      "hideMethod": "hide"
+					    }
+					    toastr.success("Done!");
+					    
+					},
+					error:function()
+					{
+						toastr.options = {
+					      "closeButton": true,
+					      "debug": false,
+					      "positionClass": "toast-top-right",
+					      "onclick": null,
+					      "showDuration": "3000",
+					      "hideDuration": "100",
+					      "timeOut": "3000",
+					      "extendedTimeOut": "0",
+					      "showEasing": "swing",
+					      "hideEasing": "swing",
+					      "showMethod": "show",
+					      "hideMethod": "hide"
+					    }
+					    toastr.error("Error! Please try again later");
+					}
+				});
+				
+			}
+			else
+			{
+				toastr.options = {
+			      "closeButton": true,
+			      "debug": false,
+			      "positionClass": "toast-top-right",
+			      "onclick": null,
+			      "showDuration": "3000",
+			      "hideDuration": "100",
+			      "timeOut": "3000",
+			      "extendedTimeOut": "0",
+			      "showEasing": "swing",
+			      "hideEasing": "swing",
+			      "showMethod": "show",
+			      "hideMethod": "hide"
+			    }
+			    toastr.warning("Please select a starting date");
+			}
+		}
+		else if(report == 'monthly')
+		{
+			if(monthly != 'mm/yyyy')		    
+			{
+				var date = new Date("01/"+monthly);
+			    var newdate = new Date(date);
+			    var smm = date.getDate();
+			    var sy = date.getFullYear();
+				$.ajax
+				({
+					url: '/monthlyCensusReport',
+					type: 'get',
+					data:  { 
+						month : smm,
+						year : sy
+					},
+					dataType : 'json',
+					success:function(response){
+						toastr.options = {
+					      "closeButton": true,
+					      "debug": false,
+					      "positionClass": "toast-top-right",
+					      "onclick": null,
+					      "showDuration": "3000",
+					      "hideDuration": "100",
+					      "timeOut": "3000",
+					      "extendedTimeOut": "0",
+					      "showEasing": "swing",
+					      "hideEasing": "swing",
+					      "showMethod": "show",
+					      "hideMethod": "hide"
+					    }
+					    toastr.success("Done!");
+					},
+					error:function()
+					{
+						toastr.options = {
+					      "closeButton": true,
+					      "debug": false,
+					      "positionClass": "toast-top-right",
+					      "onclick": null,
+					      "showDuration": "3000",
+					      "hideDuration": "100",
+					      "timeOut": "3000",
+					      "extendedTimeOut": "0",
+					      "showEasing": "swing",
+					      "hideEasing": "swing",
+					      "showMethod": "show",
+					      "hideMethod": "hide"
+					    }
+					    toastr.error("Error! Please try again later");
+					}
+
+				});
+				
+			}
+			else
+			{
+				toastr.options = {
+			      "closeButton": true,
+			      "debug": false,
+			      "positionClass": "toast-top-right",
+			      "onclick": null,
+			      "showDuration": "3000",
+			      "hideDuration": "100",
+			      "timeOut": "3000",
+			      "extendedTimeOut": "0",
+			      "showEasing": "swing",
+			      "hideEasing": "swing",
+			      "showMethod": "show",
+			      "hideMethod": "hide"
+			    }
+			    toastr.warning("Please select a Month/Year");
+			}
+
+		}
+		else if(report == 'yearly')
+		{
+			if(yearly != 'yyyy')   
+			{
+				var date = new Date("01/01/"+yearly);
+			    var newdate = new Date(date);
+			    var sy = date.getFullYear();
+				$.ajax
+				({
+					url: '/yearlyCensusReport',
+					type: 'get',
+					data:  { 
+						month : smm,
+						year : sy
+					},
+					dataType : 'json',
+					success:function(response){
+						
+						toastr.options = {
+					      "closeButton": true,
+					      "debug": false,
+					      "positionClass": "toast-top-right",
+					      "onclick": null,
+					      "showDuration": "3000",
+					      "hideDuration": "100",
+					      "timeOut": "3000",
+					      "extendedTimeOut": "0",
+					      "showEasing": "swing",
+					      "hideEasing": "swing",
+					      "showMethod": "show",
+					      "hideMethod": "hide"
+					    }
+					    toastr.success("Done!");
+					},
+					error:function()
+					{
+						toastr.options = {
+					      "closeButton": true,
+					      "debug": false,
+					      "positionClass": "toast-top-right",
+					      "onclick": null,
+					      "showDuration": "3000",
+					      "hideDuration": "100",
+					      "timeOut": "3000",
+					      "extendedTimeOut": "0",
+					      "showEasing": "swing",
+					      "hideEasing": "swing",
+					      "showMethod": "show",
+					      "hideMethod": "hide"
+					    }
+					    toastr.error("Error! Please try again later");
+					}
+				});
+				
+			}
+			else
+			{
+				toastr.options = {
+			      "closeButton": true,
+			      "debug": false,
+			      "positionClass": "toast-top-right",
+			      "onclick": null,
+			      "showDuration": "3000",
+			      "hideDuration": "100",
+			      "timeOut": "3000",
+			      "extendedTimeOut": "0",
+			      "showEasing": "swing",
+			      "hideEasing": "swing",
+			      "showMethod": "show",
+			      "hideMethod": "hide"
+			    }
+			    toastr.warning("Please select a Year");
+			}
+		}
 });
